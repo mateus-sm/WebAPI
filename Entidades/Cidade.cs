66bd70b54c0d5f258878164495a2edbff0d8be28
Mateus Maciel <85745983+mateus-sm@users.noreply.github.com>
2026-03-23 00:05:09 -0300

    ADD Cidade

---
+++ Entidades/Cidade.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Entidades
+{
+    public class Cidade
+    {
+        public int CidadeId { get; set; }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public int IBGEMunicipio { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
